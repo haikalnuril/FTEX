29d6466eaca02e08d5f29ea5eb5f4657ce88ef44
haikalnuril <haikal.nuril23@gmail.com>
2024-08-03 06:52:55 +0700

    feat: database for recording

---
+++ app/Http/Controllers/RecordController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\record;
+use App\Http\Requests\StorerecordRequest;
+use App\Http\Requests\UpdaterecordRequest;
+
+class RecordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    
+}
